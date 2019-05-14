cf2e356b6e6fe0ccb025348d6cc6dc765bdd98e2
BinkiZhao <samurai7@foxmail.com>
2019-05-14 23:22:07 +0800

    add shopping cart animation

---
+++ src/pages/motion/ShoppingCart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ShoppingCart() {
+  // 做一个商品进购物车的抛物线
+  // given: sourceLocation, targetLocation, vx -> time
+  // in the y direction, deltaY = vy * t + 1 / 2 * ay * t ^ 2
+  // if given a, we can get v0.
+  // a should be positive, v0 should be negative.
+}
+
+export default ShoppingCart;
