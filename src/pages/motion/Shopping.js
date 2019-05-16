30f37408288f5eaaefe8c7908be822992ba57f8e
BinkiZhao <samurai7@foxmail.com>
2019-05-17 00:09:54 +0800

    fix shopping x

--- src/pages/motion/Shopping.js
+++ src/pages/motion/Shopping.js
@@ -2,18 +2,22 @@ import React, { memo, useRef } from 'react';
 import Vector from '../../utils/Vector';
 import s from './Shopping.module.css';
 
-function jumpToCart(element, sourcePosition, targetPosition, done) {
+/**
+ * @param {HTMLElement} element
+ * @param {Vector} source
+ * @param {Vector} target
+ */
+function jumpToCart(element, source, target) {
   // 做一个商品进购物车的抛物线
   // given: sourceLocation, targetLocation, vx -> time
   // in the y direction, deltaY = vy * t + 1 / 2 * ay * t ^ 2
   // if given a, we can get v0.
   // a should be positive, v0 should be negative.
-  const delta = Vector.sub(targetPosition, sourcePosition);
-  const t = 60;
-  const a = new Vector(0, .5);
+  const delta = Vector.sub(target, source);
+  const t = 40;
+  const a = new Vector(0, 1.5);
   const v = new Vector(delta.x / t, (delta.y - 1 / 2 * a.y * t ** 2) / t);
-
-  const p = sourcePosition.copy();
+  const p = source.copy();
 
   const updatePosition = (position) => {
     element.style.display = 'block';
@@ -21,19 +25,19 @@ function jumpToCart(element, sourcePosition, targetPosition, done) {
   };
 
   const update = () => {
-    if (p.x > targetPosition.x) {
-      done && done();
-      return;
-    }
+    requestAnimationFrame(() => {
+      if (p.x > target.x && p.y > target.y) {
+        return;
+      }
 
-     requestAnimationFrame(update);
+      updatePosition(p);
 
-     // update position
-     updatePosition(p);
+      // calc new position
+      v.add(a);
+      p.add(v);
 
-     // calc new position
-     v.add(a);
-     p.add(v);
+      update();
+    });
   };
 
   update();
@@ -51,15 +55,16 @@ function Shopping() {
   const itemRef = useRef(null);
 
   const handleClick = () => {
-    const sourcePosition = getElementPosition(buyRef.current);
-    const targetPosition = getElementPosition(cartRef.current);
-    jumpToCart(itemRef.current, sourcePosition, targetPosition);
+    const source = getElementPosition(buyRef.current);
+    const target = getElementPosition(cartRef.current);
+    jumpToCart(itemRef.current, source, target);
   };
 
   return (
     <div className={s.root}>
       <div>
         <div ref={itemRef} className={s.item} /> 
+        <div className={s.placeholder} />
         <div className={s.placeholder} />
         <div className={s.placeholder} />
         <div className={s.placeholder} />
