17cb37e4b921f875cdde46ab5477885df22fb610
Minhui.Zhao <minhui.zhao@thoughtworks.com>
2019-06-03 18:40:41 +0800

    Updates

---
+++ precache-manifest.a7b7d24d7c36b6ab1cd2c164aa0c989c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95853e3a4338378bd295f35a047a617f",
+    "url": "/physical-animations/index.html"
+  },
+  {
+    "revision": "e0f147d742175c6105a8",
+    "url": "/physical-animations/static/css/main.681d151c.chunk.css"
+  },
+  {
+    "revision": "55e30b492bacdf3c01c3",
+    "url": "/physical-animations/static/js/2.252fa89e.chunk.js"
+  },
+  {
+    "revision": "e0f147d742175c6105a8",
+    "url": "/physical-animations/static/js/main.e193da04.chunk.js"
+  },
+  {
+    "revision": "ffe90893319c854116e3",
+    "url": "/physical-animations/static/js/runtime~main.eb99dbb0.js"
+  }
+]);
